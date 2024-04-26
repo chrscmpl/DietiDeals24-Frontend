6c032a900d5346ca1cbd7abf6da7402e76f3a403
chrscmpl <chcampolongo1@gmail.com>
2024-04-26 22:28:02 +0200

    Minor changes

--- src/app/models/auction.model.ts
+++ src/app/models/auction.model.ts
@@ -14,8 +14,9 @@ interface AuctionInterface {
   conditions: string;
   location: Location;
   timeLeft: number;
+  endTime: number;
   endDate: Date;
-  imageUrl: string;
+  images: string[];
   sum: number;
   sumDescription: string;
   auctionType: AuctionType;
@@ -23,18 +24,19 @@ interface AuctionInterface {
 
 type AuctionParameters = Omit<
   AuctionInterface,
-  'sum' | 'sumDescription' | 'auctionType' | 'endDate'
->;
+  'sum' | 'sumDescription' | 'auctionType' | 'endDate' | 'images' | 'timeLeft'
+> &
+  Partial<Pick<AuctionInterface, 'images'>>;
 
-export class Auction implements AuctionInterface {
+export abstract class Auction implements AuctionInterface {
   private _id: string;
   private _title: string;
   private _description?: string;
   private _user?: userSummary;
   private _conditions: string;
   private _location: Location;
-  private _timeLeft: number;
-  private _imageUrl: string;
+  private _endTime: number;
+  private _images: string[];
 
   constructor(auction: AuctionParameters) {
     this._id = auction.id;
@@ -43,8 +45,8 @@ export class Auction implements AuctionInterface {
     this._user = auction.user;
     this._conditions = auction.conditions;
     this._location = auction.location;
-    this._timeLeft = auction.timeLeft;
-    this._imageUrl = auction.imageUrl;
+    this._endTime = auction.endTime;
+    this._images = auction.images ?? [];
   }
 
   public get id(): string {
@@ -59,8 +61,16 @@ export class Auction implements AuctionInterface {
     return this._description;
   }
 
+  private set description(value: string) {
+    this._description = value;
+  }
+
   public get user(): userSummary | undefined {
     return this._user;
+  }
+
+  private set user(value: userSummary) {
+    this._user = value;
   }
 
   public get conditions(): string {
@@ -72,30 +82,37 @@ export class Auction implements AuctionInterface {
   }
 
   public get timeLeft(): number {
-    return this._timeLeft;
+    return this._endTime - Math.floor(Date.now() / 1000);
+  }
+
+  public get endTime(): number {
+    return this._endTime;
   }
 
   public get endDate(): Date {
     const date = new Date();
-    date.setSeconds(date.getSeconds() + this._timeLeft);
+    date.setSeconds(this._endTime);
     return date;
   }
 
-  public get imageUrl(): string {
-    return this._imageUrl;
+  public get images(): string[] {
+    return this._images;
   }
 
-  public get sum(): number {
-    return 0;
+  public abstract get sum(): number;
+
+  public abstract get sumDescription(): string;
+
+  public abstract get auctionType(): AuctionType;
+
+  private missingFields(): string[] {
+    const missing = [];
+    if (this._description === undefined) missing.push('description');
+    if (this._user === undefined) missing.push('user');
+    return missing;
   }
 
-  public get sumDescription(): string {
-    return '';
-  }
-
-  public get auctionType(): AuctionType {
-    return AuctionType.silent;
-  }
+  public abstract complete(): void;
 }
 
 export class SilentAuction extends Auction {
@@ -124,6 +141,8 @@ export class SilentAuction extends Auction {
   public override get auctionType(): AuctionType {
     return AuctionType.silent;
   }
+
+  public override complete(): void {}
 }
 
 export class ReverseAuction extends Auction {
@@ -162,4 +181,6 @@ export class ReverseAuction extends Auction {
   public override get auctionType(): AuctionType {
     return AuctionType.reverse;
   }
+
+  public override complete(): void {}
 }
