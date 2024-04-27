102d9fc09f013c07c2bf342157b4e4e1d8c8d738
chrscmpl <chcampolongo1@gmail.com>
2024-04-27 17:59:02 +0200

    Add mockup router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: LoginPageComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+];
---
+++ src/app/pages/login-page/login-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dd24-login-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.scss'
+})
+export class LoginPageComponent {
+
+}
