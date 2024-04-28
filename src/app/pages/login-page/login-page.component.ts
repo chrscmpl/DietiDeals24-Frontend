d5069618f15cfe45380055d059123e57409e46b2
fDero <francescodero@outlook.it>
2024-04-28 13:04:48 +0200

    LoginPageComponent almost done

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { AuthenticationPageComponent } from '../authentication-page/authentication-page.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'dd24-login-page',
   standalone: true,
-  imports: [],
+  imports: [AuthenticationPageComponent, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
-export class LoginPageComponent {
+export class LoginPageComponent extends AuthenticationPageComponent {
 
+  loginForm = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required, Validators.minLength(8)])
+  });
+
+  dd24Login() {
+    console.log('Login form values:', this.loginForm);
+  }
+
+  googleLogin() {
+    console.log('Google login');
+  }
 }
