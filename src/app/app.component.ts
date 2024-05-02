399e6ee9ee4dbe9d253287f4dbfc1eed9705d83c
chrscmpl <chcampolongo1@gmail.com>
2024-05-02 15:49:21 +0200

    Add lazy routing, visual loading indicators for lazy loaded routes, work on homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { Auction, SilentAuction, ReverseAuction } from './models/auction.model';
+import { Component, OnInit } from '@angular/core';
+import {
+    RouteConfigLoadEnd,
+    RouteConfigLoadStart,
+    Router,
+    RouterOutlet,
+} from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LoadingIndicator } from './helpers/loadingIndicator';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
     selector: 'dd24-root',
     standalone: true,
-    imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+    imports: [RouterOutlet, HeaderComponent, FooterComponent, AsyncPipe],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+    public isLoadingRouteIndicator = new LoadingIndicator(100);
+
+    constructor(private router: Router) {}
+
+    ngOnInit(): void {
+        this.router.events.subscribe((ev) => {
+            if (ev instanceof RouteConfigLoadStart) {
+                this.isLoadingRouteIndicator.start();
+            } else if (ev instanceof RouteConfigLoadEnd) {
+                this.isLoadingRouteIndicator.stop();
+            }
+        });
+    }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,56 @@
 import { Routes } from '@angular/router';
-import { LoginPageComponent } from './pages/login-page/login-page.component';
-import { AuthenticationPageComponent } from './pages/authentication-page/authentication-page.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { YourPageComponent } from './pages/your-page/your-page.component';
-import { CreateAuctionPageComponent } from './pages/create-auction-page/create-auction-page.component';
-import { HelpPageComponent } from './pages/help-page/help-page.component';
-import { RegistrationPageComponent } from './pages/registration-page/registration-page.component';
-import { ForgotPasswordPageComponent } from './pages/forgot-password-page/forgot-password-page.component';
 import { SearchResultsPageComponent } from './pages/search-results-page/search-results-page.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: HomePageComponent },
+    {
+        path: 'home',
+        loadComponent: () =>
+            import('./pages/home-page/home-page.component').then(
+                (m) => m.HomePageComponent,
+            ),
+    },
     { path: 'search/:category', component: SearchResultsPageComponent },
-    { path: 'your-page', component: YourPageComponent },
-    { path: 'create-auction', component: CreateAuctionPageComponent },
-    { path: 'help', component: HelpPageComponent },
-    { path: 'login', component: LoginPageComponent },
-    { path: 'register', component: RegistrationPageComponent },
-    { path: 'forgot-password', component: ForgotPasswordPageComponent },
-    { path: 'auth', component: AuthenticationPageComponent },
+    {
+        path: 'your-page',
+        loadComponent: () =>
+            import('./pages/your-page/your-page.component').then(
+                (m) => m.YourPageComponent,
+            ),
+    },
+    {
+        path: 'create-auction',
+        loadComponent: () =>
+            import(
+                './pages/create-auction-page/create-auction-page.component'
+            ).then((m) => m.CreateAuctionPageComponent),
+    },
+    {
+        path: 'help',
+        loadComponent: () =>
+            import('./pages/help-page/help-page.component').then(
+                (m) => m.HelpPageComponent,
+            ),
+    },
+    {
+        path: 'login',
+        loadComponent: () =>
+            import('./pages/login-page/login-page.component').then(
+                (m) => m.LoginPageComponent,
+            ),
+    },
+    {
+        path: 'register',
+        loadComponent: () =>
+            import(
+                './pages/registration-page/registration-page.component'
+            ).then((m) => m.RegistrationPageComponent),
+    },
+    {
+        path: 'forgot-password',
+        loadComponent: () =>
+            import(
+                './pages/forgot-password-page/forgot-password-page.component'
+            ).then((m) => m.ForgotPasswordPageComponent),
+    },
 ];
