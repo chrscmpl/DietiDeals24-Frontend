cc186f3f57d3b276f8b4f4ae3254394933a7d9a7
chrscmpl <chcampolongo1@gmail.com>
2024-05-19 19:39:02 +0200

    Add mobile UI

--- src/app/components/mobile-header/mobile-header.component.ts
+++ src/app/components/mobile-header/mobile-header.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { WindowService } from '../../services/window.service';
+import { AsyncPipe, Location } from '@angular/common';
+import { LogoComponent } from '../logo/logo.component';
 
 @Component({
     selector: 'dd24-mobile-header',
     standalone: true,
-    imports: [],
+    imports: [LogoComponent, AsyncPipe],
     templateUrl: './mobile-header.component.html',
     styleUrl: './mobile-header.component.scss',
 })
-export class MobileHeaderComponent {}
+export class MobileHeaderComponent {
+    constructor(
+        public windowService: WindowService,
+        public location: Location,
+    ) {}
+}
--- src/app/components/mobile-navbar/mobile-navbar.component.ts
+++ src/app/components/mobile-navbar/mobile-navbar.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { DockModule } from 'primeng/dock';
 
 @Component({
     selector: 'dd24-mobile-navbar',
     standalone: true,
-    imports: [],
+    imports: [DockModule],
     templateUrl: './mobile-navbar.component.html',
     styleUrl: './mobile-navbar.component.scss',
 })
-export class MobileNavbarComponent {}
+export class MobileNavbarComponent {
+    public items: MenuItem[] = [
+        { name: 'Home', routerLink: 'home', icon: 'pi pi-home' },
+        {
+            name: 'Search',
+            routerLink: 'auctions',
+            icon: 'pi pi-search',
+        },
+        {
+            name: 'Create Auction',
+            routerLink: 'create-auction',
+            icon: 'pi pi-plus',
+        },
+        { name: 'Your Page', routerLink: 'your-page', icon: 'pi pi-user' },
+        { name: 'Help', routerLink: 'help', icon: 'pi pi-question-circle' },
+    ];
+}
