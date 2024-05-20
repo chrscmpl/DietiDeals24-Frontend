c1bac82dd066609aa4e0519c3081eac267107951
chrscmpl <chcampolongo1@gmail.com>
2024-05-20 22:09:21 +0200

    Stepper skeleton

--- src/app/pages/registration-page/registration-page.component.ts
+++ src/app/pages/registration-page/registration-page.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { StepperModule } from 'primeng/stepper';
 
 @Component({
     selector: 'dd24-registration-page',
     standalone: true,
-    imports: [],
+    imports: [StepperModule, ButtonModule],
     templateUrl: './registration-page.component.html',
     styleUrl: './registration-page.component.scss',
 })
-export class RegistrationPageComponent {}
+export class RegistrationPageComponent {
+    public activeStep: number = 0;
+
+    public nextStep(): void {
+        if (this.activeStep < 2) this.activeStep++;
+    }
+
+    public prevStep(): void {
+        if (this.activeStep > 0) this.activeStep--;
+    }
+}
