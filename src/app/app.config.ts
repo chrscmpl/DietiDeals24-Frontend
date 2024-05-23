c9ac05b76d5dc6dad8423af56a0d8de708df5106
chrscmpl <chcampolongo1@gmail.com>
2024-05-24 00:24:48 +0200

    Add PWA

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,27 +9,32 @@ import { CurrencyPipe } from '@angular/common';
 import { IntervalPipe } from './pipes/interval.pipe';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authorizationInterceptor } from './interceptors/authorization.interceptor';
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, isDevMode } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuctionsRequestGuard } from './guards/auctions-request.guard';
 import { HideUIGuard } from './guards/hide-ui.guard';
 import { ShowUIGuard } from './guards/show-ui.guard';
 import { AuthenticationGuard } from './guards/authentication.guard';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        provideRouter(routes),
-        provideHttpClient(withInterceptors([authorizationInterceptor])),
-        importProvidersFrom([BrowserAnimationsModule]),
-        Location,
-        LocalDatePipe,
-        OneCharUpperPipe,
-        DatePipe,
-        IntervalPipe,
-        CurrencyPipe,
-        AuctionsRequestGuard,
-        HideUIGuard,
-        ShowUIGuard,
-        AuthenticationGuard,
-    ],
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([authorizationInterceptor])),
+    importProvidersFrom([BrowserAnimationsModule]),
+    Location,
+    LocalDatePipe,
+    OneCharUpperPipe,
+    DatePipe,
+    IntervalPipe,
+    CurrencyPipe,
+    AuctionsRequestGuard,
+    HideUIGuard,
+    ShowUIGuard,
+    AuthenticationGuard,
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+],
 };
