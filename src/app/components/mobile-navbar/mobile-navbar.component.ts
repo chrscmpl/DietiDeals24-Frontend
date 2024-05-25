600639beb532f7899a0fa7fe47f0defeab3654ad
chrscmpl <chcampolongo1@gmail.com>
2024-05-25 13:33:05 +0200

    Changes mobile navbar

--- src/app/components/mobile-navbar/mobile-navbar.component.ts
+++ src/app/components/mobile-navbar/mobile-navbar.component.ts
@@ -1,30 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AvatarModule } from 'primeng/avatar';
 import { AuthenticationService } from '../../services/authentication.service';
-import { AsyncPipe } from '@angular/common';
-import { Observable, map } from 'rxjs';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'dd24-mobile-navbar',
     standalone: true,
-    imports: [AvatarModule, RouterLink, AsyncPipe],
+    imports: [AvatarModule, RouterLink],
     templateUrl: './mobile-navbar.component.html',
     styleUrl: './mobile-navbar.component.scss',
 })
-export class MobileNavbarComponent {
+export class MobileNavbarComponent implements OnInit, OnDestroy {
+    private subscriptions: Subscription[] = [];
+    public profilePicture: string | null = null;
+
     public constructor(
         private readonly authenticationService: AuthenticationService,
     ) {}
 
-    public profilePicture$: Observable<string | null> =
-        this.authenticationService.isLogged$.pipe(
-            map((isLogged) => {
-                if (!isLogged) return null;
-                return (
+    ngOnInit(): void {
+        this.subscriptions.push(
+            this.authenticationService.isLogged$.subscribe((isLogged) => {
+                if (!isLogged) this.profilePicture = null;
+                this.profilePicture =
                     this.authenticationService.loggedUser?.profilePictureUrl ??
-                    null
-                );
+                    null;
             }),
         );
+    }
+
+    ngOnDestroy(): void {
+        this.subscriptions.forEach((s) => s.unsubscribe());
+    }
 }
