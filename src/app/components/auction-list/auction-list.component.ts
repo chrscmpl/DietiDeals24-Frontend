1e5c670fddc22fa4c2ec6be7ba0730cca8b96f22
chrscmpl <chcampolongo1@gmail.com>
2024-07-18 13:38:17 +0200

    Auction list loading indicator

--- src/app/components/auction-list/auction-list.component.ts
+++ src/app/components/auction-list/auction-list.component.ts
@@ -4,11 +4,19 @@ import { AuctionCardComponent } from '../auction-card/auction-card.component';
 import { PaginatedRequest } from '../../helpers/paginatedRequest';
 import { ReloadButtonComponent } from '../reload-button/reload-button.component';
 import { Subscription } from 'rxjs';
+import { LoadingPlaceholderComponent } from '../loading-placeholder/loading-placeholder.component';
+import { LoadingIndicator } from '../../helpers/loadingIndicator';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
     selector: 'dd24-auction-list',
     standalone: true,
-    imports: [AuctionCardComponent, ReloadButtonComponent],
+    imports: [
+        AuctionCardComponent,
+        ReloadButtonComponent,
+        LoadingPlaceholderComponent,
+        AsyncPipe,
+    ],
     templateUrl: './auction-list.component.html',
     styleUrl: './auction-list.component.scss',
 })
@@ -19,41 +27,63 @@ export class AuctionListComponent implements OnInit, OnDestroy {
     private dataSubscription!: Subscription | null;
     public auctions: AuctionSummary[] = [];
     public error: boolean = false;
+    public showEmpty: boolean = false;
+    public loadingIndicator: LoadingIndicator = new LoadingIndicator(1000);
 
     public constructor() {}
 
-    ngOnInit(): void {
+    public ngOnInit(): void {
         this.subscribeToData();
-        this.request?.more();
+        this.more();
     }
 
-    ngOnDestroy(): void {
+    public ngOnDestroy(): void {
         this.dataSubscription?.unsubscribe();
     }
 
-    scrolled(index: number): void {
+    public scrolled(index: number): void {
         if (index === this.auctions.length - 1) {
-            this.request?.more();
+            this.more();
         }
     }
 
-    reloadAfterError(): void {
+    public more(): void {
+        this.startLoading();
+        this.request?.more();
+    }
+
+    public reloadAfterError(): void {
         this.request?.refresh();
         this.subscribeToData();
-        this.request?.more();
+        this.more();
     }
 
     private subscribeToData(): void {
         this.dataSubscription =
             this.request?.data$.subscribe({
                 next: (auctions) => {
-                    this.error = false;
                     this.auctions.push(...auctions);
+                    this.error = false;
+                    this.stopLoading();
                 },
                 error: (err) => {
+                    this.stopLoading();
                     this.error = true;
                     console.error(err);
                 },
+                complete: () => {
+                    this.stopLoading();
+                },
             }) ?? null;
+    }
+
+    private startLoading(): void {
+        this.loadingIndicator.start();
+        this.showEmpty = false;
+    }
+
+    private stopLoading(): void {
+        this.loadingIndicator.stop();
+        this.showEmpty = true;
     }
 }
