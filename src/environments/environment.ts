184410a4724c53d9e2f7a905456bd3877baab37f
chrscmpl <chcampolongo1@gmail.com>
2024-08-09 07:23:38 +0200

    Add script to deploy to github pages

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     isProd: true,
-    backendHost: 'http://localhost:8080',
+    backendHost: 'http://192.168.1.21:8080',
     disableWarnings: false,
     passwordPattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\W).+$/,
     passwordMediumPattern: '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)(?=.{8,})',
