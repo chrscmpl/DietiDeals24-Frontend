47854a65ba9945045770dc3bb4583a6dd2ac18ce
chrscmpl <96392458+chrscmpl@users.noreply.github.com>
2024-08-10 12:13:41 +0200

    Fixes auction cards and mobile header

--- src/app/components/mobile-header/mobile-header.component.ts
+++ src/app/components/mobile-header/mobile-header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, NgZone, Renderer2 } from '@angular/core';
 import { WindowService } from '../../services/window.service';
 import { AsyncPipe, Location } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
@@ -15,14 +15,12 @@ import { delay, map, Observable, shareReplay, startWith, tap } from 'rxjs';
     styleUrl: './mobile-header.component.scss',
 })
 export class MobileHeaderComponent {
-    public longTitle: boolean = false;
-
     public title$: Observable<string> =
         this.navigationService.navigationEnd$.pipe(
             startWith(null),
             delay(50),
             map(() => this.titleService.getTitle()),
-            tap((title) => (this.longTitle = title.length > 18)),
+            tap(this.adjustTitleSize.bind(this)),
             shareReplay(1),
         );
 
@@ -31,5 +29,21 @@ export class MobileHeaderComponent {
         public readonly location: Location,
         public readonly titleService: Title,
         private readonly navigationService: NavigationService,
+        private readonly element: ElementRef,
+        private readonly renderer: Renderer2,
+        private readonly zone: NgZone,
     ) {}
+
+    private adjustTitleSize() {
+        this.zone.runOutsideAngular(() => {
+            setTimeout(() => {
+                const title =
+                    this.element.nativeElement.querySelector('.title');
+                if (!title) return;
+                if (title.clientHeight < title.scrollHeight)
+                    this.renderer.addClass(title, 'title-long');
+                else this.renderer.removeClass(title, 'title-long');
+            }, 50);
+        });
+    }
 }
