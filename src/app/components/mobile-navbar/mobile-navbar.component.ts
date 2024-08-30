8675d4a2044535aa5d3ac889fb3be3372739cb21
chrscmpl <96392458+chrscmpl@users.noreply.github.com>
2024-08-30 18:00:09 +0200

    Fixes mobile navbar

--- src/app/components/mobile-navbar/mobile-navbar.component.ts
+++ src/app/components/mobile-navbar/mobile-navbar.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AvatarModule } from 'primeng/avatar';
 import { AuthenticationService } from '../../services/authentication.service';
-import { Observable, map, startWith } from 'rxjs';
+import { Observable, Subscription, map, startWith } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { BadgeModule } from 'primeng/badge';
 import { NotificationsService } from '../../services/notifications.service';
@@ -20,13 +20,26 @@ import { NotificationsService } from '../../services/notifications.service';
     templateUrl: './mobile-navbar.component.html',
     styleUrl: './mobile-navbar.component.scss',
 })
-export class MobileNavbarComponent {
-    public showImagePlaceholder: boolean = false;
+export class MobileNavbarComponent implements OnInit, OnDestroy {
+    private subscriptions: Subscription[] = [];
+    public showImagePlaceholder: boolean = true;
 
     public constructor(
         public readonly authenticationService: AuthenticationService,
         public readonly notificationsService: NotificationsService,
     ) {}
+
+    public ngOnInit(): void {
+        this.subscriptions.push(
+            this.profilePicture$.subscribe(
+                (pic) => (this.showImagePlaceholder = !pic),
+            ),
+        );
+    }
+
+    public ngOnDestroy(): void {
+        this.subscriptions.forEach((sub) => sub.unsubscribe());
+    }
 
     public readonly profilePicture$: Observable<string | null> =
         this.authenticationService.loggedUser$.pipe(
