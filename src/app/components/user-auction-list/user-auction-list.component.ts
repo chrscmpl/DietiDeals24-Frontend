baae08d50d56bab18ed12af806ac80a70ad4b63d
chrscmpl <96392458+chrscmpl@users.noreply.github.com>
2024-09-11 13:30:41 +0200

    UserAuctionListComponent

--- src/app/components/user-auction-list/user-auction-list.component.ts
+++ src/app/components/user-auction-list/user-auction-list.component.ts
@@ -1,23 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuctionListComponent } from '../auction-list/auction-list.component';
 import { ActivatedRoute } from '@angular/router';
 import {
     auctionsPaginationParams,
     AuctionsService,
 } from '../../services/auctions.service';
+import { omit } from 'lodash-es';
+import { distinctUntilChanged, Subscription, take } from 'rxjs';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+type filterValue = null | 'auctions' | 'bids';
 
 @Component({
     selector: 'dd24-user-auction-list',
     standalone: true,
-    imports: [AuctionListComponent],
+    imports: [AuctionListComponent, DropdownModule, ReactiveFormsModule],
     templateUrl: './user-auction-list.component.html',
     styleUrl: './user-auction-list.component.scss',
 })
-export class UserAuctionListComponent implements OnInit {
+export class UserAuctionListComponent implements OnInit, OnDestroy {
+    private readonly subscriptions: Subscription[] = [];
     public requestKey!: string;
+    private originalKey!: string;
     public params!: auctionsPaginationParams;
 
-    public filter: null | 'auctions' | 'bids' = null;
+    public enableFilter: boolean = false;
+    public filterControl = new FormControl<filterValue>(null);
+
+    public filterOptions = [
+        { label: 'Bids and auctions', value: null },
+        { label: 'Bids only', value: 'bids' },
+        { label: 'Auctions only', value: 'auctions' },
+    ];
 
     public constructor(
         private readonly route: ActivatedRoute,
@@ -25,15 +40,55 @@ export class UserAuctionListComponent implements OnInit {
     ) {}
 
     public ngOnInit(): void {
-        this.requestKey = this.route.snapshot.data['auctionsRequestData'].key;
-        this.params = this.route.snapshot.data['auctionsRequestData'];
+        this.route.data.pipe(take(1)).subscribe((data) => {
+            this.params = omit(
+                data['auctionsRequestData'],
+                'key',
+            ) as auctionsPaginationParams;
 
-        this.filter = this.params.onlyAuctions
-            ? 'auctions'
-            : this.params.onlyBids
-              ? 'bids'
-              : null;
+            this.originalKey = data['auctionsRequestData'].key;
 
-        this.auctionsService.setIfAbsent(this.requestKey, this.params);
+            this.requestKey = this.completeKey(this.originalKey, this.params);
+
+            this.initFilter();
+
+            this.auctionsService.setIfAbsent(this.requestKey, this.params);
+        });
+    }
+
+    public ngOnDestroy(): void {
+        this.subscriptions.forEach((sub) => sub.unsubscribe());
+    }
+
+    private initFilter() {
+        this.enableFilter = !this.params.ofUser || !this.params.currentAuctions;
+
+        this.filterControl.setValue(
+            this.params.onlyAuctions
+                ? 'auctions'
+                : this.params.onlyBids
+                  ? 'bids'
+                  : null,
+        );
+
+        this.subscriptions.push(
+            this.filterControl.valueChanges
+                .pipe(distinctUntilChanged())
+                .subscribe(this.onFilterChange.bind(this)),
+        );
+    }
+
+    private onFilterChange(value: filterValue): void {
+        this.params.onlyAuctions = value === 'auctions';
+        this.params.onlyBids = value === 'bids';
+        const key = this.completeKey(this.originalKey, this.params);
+        this.auctionsService.setIfAbsent(key, this.params);
+        this.requestKey = key;
+    }
+
+    private completeKey(key: string, params: auctionsPaginationParams): string {
+        return `${key}${
+            params.onlyAuctions ? '/auctions' : params.onlyBids ? '/bids' : ''
+        }`;
     }
 }
