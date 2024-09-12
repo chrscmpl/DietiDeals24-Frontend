a07fdd8b7f74a4386b0251740bf47b5b830d4ab1
chrscmpl <96392458+chrscmpl@users.noreply.github.com>
2024-09-12 14:36:23 +0200

    Your page, activity tab mobile layout

--- src/app/pages/your-page/activity-page/activity-page.component.ts
+++ src/app/pages/your-page/activity-page/activity-page.component.ts
@@ -4,6 +4,9 @@ import { UserInformationCardComponent } from '../../../components/user-informati
 import { AuthenticationService } from '../../../services/authentication.service';
 import { AsyncPipe } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { MenuItem } from 'primeng/api';
+import { WindowService } from '../../../services/window.service';
 
 @Component({
     selector: 'dd24-activity-page',
@@ -14,10 +17,25 @@ import { ButtonModule } from 'primeng/button';
         AsyncPipe,
         ButtonModule,
         RouterLink,
+        TabMenuModule,
     ],
     templateUrl: './activity-page.component.html',
     styleUrl: './activity-page.component.scss',
 })
 export class ActivityPageComponent {
-    public constructor(public readonly authentication: AuthenticationService) {}
+    public readonly tabs: MenuItem[] = [
+        {
+            label: 'Current',
+            routerLink: ['current'],
+        },
+        {
+            label: 'History',
+            routerLink: ['past'],
+        },
+    ];
+
+    public constructor(
+        public readonly authentication: AuthenticationService,
+        public readonly windowService: WindowService,
+    ) {}
 }
