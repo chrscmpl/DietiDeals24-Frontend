0499fed39ee6959a4a1ed35e6b3ec656d08963ec
chrscmpl <96392458+chrscmpl@users.noreply.github.com>
2024-09-13 23:17:44 +0200

    Your data page skeleton

--- src/app/pages/your-page/your-data-page/your-data-page.component.ts
+++ src/app/pages/your-page/your-data-page/your-data-page.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
-  selector: 'dd24-your-data-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './your-data-page.component.html',
-  styleUrl: './your-data-page.component.scss'
+    selector: 'dd24-your-data-page',
+    standalone: true,
+    imports: [],
+    templateUrl: './your-data-page.component.html',
+    styleUrl: './your-data-page.component.scss',
 })
 export class YourDataPageComponent {
-
+    public readonly tabs: MenuItem[] = [
+        {
+            label: 'Public profile',
+        },
+        {
+            label: 'Links',
+        },
+        {
+            label: 'Private information',
+        },
+        {
+            label: 'Payment methods',
+        },
+        {
+            label: 'Security & Privacy',
+        },
+    ];
 }
