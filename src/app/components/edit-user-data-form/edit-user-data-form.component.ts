901ba3c49f581228ac4267e3751e40498256b662
chrscmpl <96392458+chrscmpl@users.noreply.github.com>
2024-09-14 15:17:47 +0200

    Add mobile layout for your data and security&privacy pages

--- src/app/components/edit-user-data-form/edit-user-data-form.component.ts
+++ src/app/components/edit-user-data-form/edit-user-data-form.component.ts
@@ -1,14 +1,23 @@
 import { Component, Input } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { MenuModule } from 'primeng/menu';
+import { WindowService } from '../../services/window.service';
+import { AsyncPipe } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { NavigationService } from '../../services/navigation.service';
 
 @Component({
     selector: 'dd24-edit-user-data-form',
     standalone: true,
-    imports: [MenuModule],
+    imports: [MenuModule, AsyncPipe, ButtonModule],
     templateUrl: './edit-user-data-form.component.html',
     styleUrl: './edit-user-data-form.component.scss',
 })
 export class EditUserDataFormComponent {
     @Input() public tabs: MenuItem[] = [];
+
+    public constructor(
+        public readonly windowService: WindowService,
+        public readonly navigation: NavigationService,
+    ) {}
 }
