be74c84262c51ba490728fb9fcd6b4c5d85d7f26
chrscmpl <96392458+chrscmpl@users.noreply.github.com>
2024-09-16 18:52:17 +0200

    Edit password form

--- src/app/pages/your-page/security-and-privacy-page/security-and-privacy-page.component.ts
+++ src/app/pages/your-page/security-and-privacy-page/security-and-privacy-page.component.ts
@@ -10,6 +10,25 @@ import { ButtonModule } from 'primeng/button';
 import { MaskedPipe } from '../../../pipes/masked.pipe';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
+import {
+    AbstractControl,
+    FormBuilder,
+    FormControl,
+    FormGroup,
+    ReactiveFormsModule,
+    ValidationErrors,
+    Validators,
+} from '@angular/forms';
+import { environment } from '../../../../environments/environment';
+import { InputComponent } from '../../../components/input/input.component';
+import { PasswordModule } from 'primeng/password';
+import { reactiveFormsUtils } from '../../../helpers/reactive-forms-utils';
+
+interface EditPasswordForm {
+    oldPassword: FormControl<string | null>;
+    newPassword: FormControl<string | null>;
+    confirmNewPassword: FormControl<string | null>;
+}
 
 @Component({
     selector: 'dd24-security-and-privacy-page',
@@ -21,13 +40,19 @@ import { InputIconModule } from 'primeng/inputicon';
         MaskedPipe,
         IconFieldModule,
         InputIconModule,
+        ReactiveFormsModule,
+        InputComponent,
+        PasswordModule,
     ],
     templateUrl: './security-and-privacy-page.component.html',
     styleUrl: './security-and-privacy-page.component.scss',
 })
 export class SecurityAndPrivacyPageComponent implements OnInit {
+    public editPasswordForm!: FormGroup<EditPasswordForm>;
     public user!: AuthenticatedUser;
     public maskEmail = true;
+
+    public readonly environment = environment;
 
     public readonly tabs: MenuItem[] = [
         {
@@ -47,6 +72,7 @@ export class SecurityAndPrivacyPageComponent implements OnInit {
     public constructor(
         public readonly windowService: WindowService,
         private readonly route: ActivatedRoute,
+        private readonly formBuilder: FormBuilder,
     ) {}
 
     public ngOnInit(): void {
@@ -54,9 +80,64 @@ export class SecurityAndPrivacyPageComponent implements OnInit {
             this.termsOfServiceText = data['tos'];
             this.user = data['userData'];
         });
+
+        this.initForm();
+    }
+
+    public editPassword(): void {
+        if (this.editPasswordForm.invalid) {
+            reactiveFormsUtils.markAllAsDirty(this.editPasswordForm);
+            return;
+        }
+    }
+
+    public showEditPasswordForm(): void {
+        this.editPasswordForm.enable();
+    }
+
+    public hideEditPasswordForm(): void {
+        this.editPasswordForm.disable();
+        this.editPasswordForm.reset();
+    }
+
+    private initForm() {
+        this.editPasswordForm = this.formBuilder.group({
+            oldPassword: new FormControl<string | null>(null, {
+                validators: [
+                    Validators.required,
+                    Validators.minLength(8),
+                    Validators.pattern(environment.passwordPattern),
+                ],
+                updateOn: 'blur',
+            }),
+            newPassword: new FormControl<string | null>(null, {
+                validators: [
+                    Validators.required,
+                    Validators.minLength(8),
+                    Validators.pattern(environment.passwordPattern),
+                ],
+                updateOn: 'blur',
+            }),
+            confirmNewPassword: new FormControl<string | null>(null, {
+                validators: [
+                    Validators.required,
+                    this.validateConfirmPassword.bind(this),
+                ],
+                updateOn: 'blur',
+            }),
+        });
+        this.editPasswordForm.disable();
     }
 
     public toggleEmailMask(): void {
         this.maskEmail = !this.maskEmail;
+    }
+
+    private validateConfirmPassword(
+        control: AbstractControl<string>,
+    ): ValidationErrors {
+        if (control.value === this.editPasswordForm?.controls.newPassword.value)
+            return {};
+        return { passwordsDoNotMatch: true };
     }
 }
